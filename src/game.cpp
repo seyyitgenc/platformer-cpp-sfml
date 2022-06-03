13756c9c5554d2425211fb7e9a375295595346a0
seyyitgenc <mahluk053@gmail.com>
2022-06-03 20:56:20 +0300

    arbitrary collision detection

--- src/game.cpp
+++ src/game.cpp
@@ -4,59 +4,184 @@
 #include "tilemap.hpp"
 #include <vector>
 #include <algorithm>
-bool PointvsRect(const sf::Vector2i &p, const sf::RectangleShape &r)
+#include <cmath>
+
+void createShape(sf::Vector2f size, sf::Vector2f pos, sf::Color color, sf::RenderWindow *window)
 {
-    return (p.x >= r.getPosition().x && p.y >= r.getPosition().y && p.x < r.getPosition().x + r.getSize().x && p.y < r.getPosition().y + r.getSize().y);
+    sf::RectangleShape shape;
+    shape.setSize(size);
+    shape.setPosition(pos);
+    shape.setFillColor(color);
+    window->draw(shape);
+}
+struct rect
+{
+    sf::Vector2f size;
+    sf::Vector2f pos;
+    sf::Vector2f vel;
+    std::array<rect *, 4> contact;
+};
+bool RectvsRect(const rect &r1, const rect &r2)
+{
+    return (r1.pos.x < r2.pos.x + r2.size.x && r1.pos.x + r1.size.x > r2.pos.x &&
+            r1.pos.y < r2.pos.y + r2.size.y && r1.pos.y + r1.size.y > r2.pos.y);
 }
 
-bool RectvsRect(const sf::RectangleShape &r1, const sf::RectangleShape &r2)
-{
-    return (r1.getPosition().x < r2.getPosition().x + r2.getSize().x && r1.getPosition().x + r1.getSize().x > r2.getPosition().x &&
-            r1.getPosition().y < r2.getPosition().y + r2.getSize().y && r1.getPosition().y + r1.getSize().y > r2.getPosition().y);
-}
-
-bool RayvsRect(const sf::Vector2f &ray_origin, const sf::Vector2f &ray_dir, const sf::RectangleShape &target,
-               sf::Vector2f &contact_point, sf::Vector2f &contact_normal, float &t_hit_near)
+bool RayVsRect(const sf::Vector2f &ray_origin, const sf::Vector2f &ray_dir, const rect *target, sf::Vector2f &contact_point, sf::Vector2f &contact_normal, float &t_hit_near)
 {
     contact_normal = {0, 0};
     contact_point = {0, 0};
-    float t_nearX = (target.getPosition().x - ray_origin.x) / ray_dir.x;
-    float t_farX = (target.getPosition().x + target.getSize().x - ray_origin.x) / ray_dir.x;
-    float t_nearY = (target.getPosition().y - ray_origin.y) / ray_dir.y;
-    float t_farY = (target.getPosition().y + target.getSize().y - ray_origin.y) / ray_dir.y;
-    if (t_nearX > t_farX)
-        std::swap(t_nearX, t_farX);
-    if (t_nearY > t_farY)
-        std::swap(t_nearY, t_farY);
-    if (t_nearX > t_farY || t_nearY > t_farX)
+
+    // Cache division
+    sf::Vector2f invdir = {1.0f / ray_dir.x, 1.0f / ray_dir.y};
+
+    // Calculate intersections with rectangle bounding axes
+    sf::Vector2f t_near = {(target->pos.x - ray_origin.x) * invdir.x, (target->pos.y - ray_origin.y) * invdir.y};
+    sf::Vector2f t_far = {(target->pos.x + target->size.x - ray_origin.x) * invdir.x, (target->pos.y + target->size.y - ray_origin.y) * invdir.y};
+
+    if (std::isnan(t_far.y) || std::isnan(t_far.x))
         return false;
-    t_hit_near = std::max(t_nearX, t_nearY);
-    float t_hit_far = std::min(t_farX, t_farY);
+    if (std::isnan(t_near.y) || std::isnan(t_near.x))
+        return false;
+
+    // Sort distances
+    if (t_near.x > t_far.x)
+        std::swap(t_near.x, t_far.x);
+    if (t_near.y > t_far.y)
+        std::swap(t_near.y, t_far.y);
+
+    // Early rejection
+    if (t_near.x > t_far.y || t_near.y > t_far.x)
+        return false;
+
+    // Closest 'time' will be the first contact
+    t_hit_near = std::max(t_near.x, t_near.y);
+
+    // Furthest 'time' is contact on opposite side of target
+    float t_hit_far = std::min(t_far.x, t_far.y);
+
+    // Reject if ray direction is pointing away from object
     if (t_hit_far < 0)
         return false;
+
+    // Contact point of collision from parametric line equation
     contact_point = ray_origin + t_hit_near * ray_dir;
-    if (t_nearX > t_nearY)
-        (ray_dir.x < 0) ? contact_normal = {1, 0} : contact_normal = {-1, 0};
-    else if (t_nearX < t_nearY)
-        (ray_dir.y < 0) ? contact_normal = {0, 1} : contact_normal = {0, -1};
+
+    if (t_near.x > t_near.y)
+        if (invdir.x < 0)
+            contact_normal = {1, 0};
+        else
+            contact_normal = {-1, 0};
+    else if (t_near.x < t_near.y)
+        if (invdir.y < 0)
+            contact_normal = {0, 1};
+        else
+            contact_normal = {0, -1};
+
+    // Note if t_near == t_far, collision is principly in a diagonal
+    // so pointless to resolve. By returning a CN={0,0} even though its
+    // considered a hit, the resolver wont change anything.
     return true;
 }
-
-bool DynamicRectvsRect(const Entity &in, const sf::RectangleShape &target, sf::Vector2f &contact_point, sf::Vector2f &contact_normal, float dt, float &contact_time)
+bool DynamicRectVsRect(rect *r_dynamic, const float fTimeStep, rect &r_static,
+                       sf::Vector2f &contact_point, sf::Vector2f &contact_normal, float &contact_time)
 {
-    if (in.velocity.x == 0 && in.velocity.y == 0)
+    // Check if dynamic rectangle is actually moving - we assume rectangles are NOT in collision to start
+    if (r_dynamic->vel.x == 0 && r_dynamic->vel.y == 0)
         return false;
-    sf::RectangleShape expanded_Target;
-    expanded_Target.setPosition(target.getPosition() - sf::Vector2f(in.rect.getSize().x / 2, in.rect.getSize().y / 2));
-    expanded_Target.setSize(target.getSize() + in.rect.getSize());
-    if (RayvsRect(in.rect.getPosition() + sf::Vector2f(in.rect.getSize().x / 2, in.rect.getSize().y / 2),
-                  in.velocity * dt, expanded_Target, contact_point, contact_normal, contact_time))
+
+    // Expand target rectangle by source dimensions
+    rect expanded_target;
+    expanded_target.pos = r_static.pos - sf::Vector2f(r_dynamic->size.x / 2, r_dynamic->size.y / 2);
+    expanded_target.size = r_static.size + r_dynamic->size;
+
+    if (RayVsRect(r_dynamic->pos + sf::Vector2f(r_dynamic->size.x / 2, r_dynamic->size.y / 2), r_dynamic->vel * fTimeStep, &expanded_target, contact_point, contact_normal, contact_time))
+        return (contact_time >= 0.0f && contact_time < 1.0f);
+    else
+        return false;
+}
+bool ResolveDynamicRectVsRect(rect *r_dynamic, const float fTimeStep, rect *r_static)
+{
+    sf::Vector2f contact_point, contact_normal;
+    float contact_time = 0.0f;
+    if (DynamicRectVsRect(r_dynamic, fTimeStep, *r_static, contact_point, contact_normal, contact_time))
     {
-        if(contact_time<=1.0f)
-            return true;
+        if (contact_normal.y > 0)
+            r_dynamic->contact[0] = r_static;
+        else
+            nullptr;
+        if (contact_normal.x < 0)
+            r_dynamic->contact[1] = r_static;
+        else
+            nullptr;
+        if (contact_normal.y < 0)
+            r_dynamic->contact[2] = r_static;
+        else
+            nullptr;
+        if (contact_normal.x > 0)
+            r_dynamic->contact[3] = r_static;
+        else
+            nullptr;
+
+        r_dynamic->vel += sf::Vector2f(contact_normal.x * std::abs(r_dynamic->vel.x), contact_normal.y * std::abs(r_dynamic->vel.y)) * (1 - contact_time);
+        return true;
     }
+
     return false;
 }
+
+class test
+{
+public:
+    std::vector<rect> vRects;
+    bool OnUserCreate()
+    {
+        vRects.push_back({{50.0f, 50.0f}, {10.0f, 40.0f}, {0.0f, 0.0f}});
+        vRects.push_back({{170.0f, 70.0f}, {200.0f, 200.0f}, {0.0f, 0.0f}});
+        vRects.push_back({{100.0f, 70.0f}, {300.0f, 270.0f}, {0.0f, 0.0f}});
+        return true;
+    }
+
+    bool OnUserUpdate(float fElapsedTime, sf::RenderWindow *window)
+    {
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::W))
+            vRects[0].vel.y = -100.0f;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::S))
+            vRects[0].vel.y = +100.0f;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::A))
+            vRects[0].vel.x = -100.0f;
+        if (sf::Keyboard::isKeyPressed(sf::Keyboard::D))
+            vRects[0].vel.x = +100.0f;
+        // Draw all rectangles
+
+        // Sort collisions in order of distance
+        sf::Vector2f cp, cn;
+        float t = 0, min_t = INFINITY;
+        std::vector<std::pair<int, float>> z;
+
+        // Work out collision point, add it to vector along with rect ID
+        for (size_t i = 1; i < vRects.size(); i++)
+        {
+            if (DynamicRectVsRect(&vRects[0], fElapsedTime, vRects[i], cp, cn, t))
+            {
+                z.push_back({i, t});
+            }
+        }
+
+        // Do the sort
+        std::sort(z.begin(), z.end(), [](const std::pair<int, float> &a, const std::pair<int, float> &b)
+                  { return a.second < b.second; });
+
+        // Now resolve the collision in correct order
+        for (auto j : z)
+            ResolveDynamicRectVsRect(&vRects[0], fElapsedTime, &vRects[j.first]);
+        // UPdate the player rectangles position, with its modified velocity
+        vRects[0].pos += vRects[0].vel * fElapsedTime;
+        return true;
+    }
+
+private:
+};
 
 void gameBuild()
 {
@@ -65,53 +190,15 @@ void gameBuild()
     sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "Hu-hunt");
     window.setFramerateLimit(120);
 
-    std::vector<sf::RectangleShape> ColMap;
-
     // player
-    Entity player;
-
     // collision test
-    sf::RectangleShape rect;
-    sf::RectangleShape mouseRect;
-    sf::VertexArray ray(sf::Lines, 2);
-    ray[0].position = sf::Vector2f(800, 60);
-    ray[0].color = sf::Color::Blue;
-    ray[1].color = sf::Color::Blue;
 
-    sf::CircleShape circle(70);
-    circle.setRadius(8);
-    rect.setSize(sf::Vector2f(300, 100));
-    rect.setPosition(sf::Vector2f(200, 400));
-    mouseRect.setSize(sf::Vector2f(30, 100));
-    mouseRect.setFillColor(sf::Color::Red);
-    // loading TileMap
-    TileMap _tileMap;
-    _tileMap.load(ColMap);
-
-    sf::Vector2f ray_direction;
+    sf::Vector2f cp, cn;
+    float ct;
+    test t;
+    t.OnUserCreate();
     while (window.isOpen())
     {
-
-        // mouse position
-        sf::Vector2i vMouse = sf::Mouse::getPosition(window);
-        // collision
-        // PointvsRect(vMouse, rect) ? rect.setFillColor(sf::Color::Yellow) : rect.setFillColor(sf::Color::White);
-        // RectvsRect(mouseRect, rect) ? rect.setFillColor(sf::Color::Yellow) : rect.setFillColor(sf::Color::White);
-        // mouseRect.setPosition((sf::Vector2f)vMouse);
-        ray[1].position = sf::Vector2f((sf::Vector2i)vMouse);
-        ray_direction = ray[1].position - ray[0].position;
-        sf::Vector2f cp, cn;
-        float t;
-        if (RayvsRect(ray[0].position, ray_direction, rect, cp, cn, t) && t <= 1.0f)
-        {
-            circle.setFillColor(sf::Color::Green);
-            circle.setPosition(cp);
-            rect.setFillColor(sf::Color::Yellow);
-        }
-        else
-        {
-            rect.setFillColor(sf::Color::White);
-        }
         // clock
         dt = clock.restart().asSeconds();
         sf::Event event;
@@ -123,29 +210,10 @@ void gameBuild()
                 window.close();
             }
         }
-        sf::FloatRect nexPos;
-        player.Update(dt);
-
-        // window collision
-        //  left collision
-        if (player.rect.getPosition().x < 0.f)
-            player.rect.setPosition(0.f, player.rect.getPosition().y);
-        // right collision
-        if (player.rect.getPosition().x > WIDTH - player.rect.getGlobalBounds().width)
-            player.rect.setPosition(WIDTH - player.rect.getGlobalBounds().width, player.rect.getPosition().y);
-        // bottom collision
-        if (player.rect.getPosition().y > HEIGHT - player.rect.getGlobalBounds().height)
-            player.rect.setPosition(player.rect.getPosition().x, HEIGHT - player.rect.getGlobalBounds().height);
-        // top collision
-        if (player.rect.getPosition().y < 0.f)
-            player.rect.setPosition(player.rect.getPosition().x, 0.f);
-
-        // drawing
         window.clear();
-        window.draw(player);
-        window.draw(rect);
-        window.draw(ray);
-        window.draw(circle);
+        for (const auto &r : t.vRects)
+            createShape(r.size, r.pos, sf::Color::Blue, &window);
+        t.OnUserUpdate(dt, &window);
         window.display();
     }
 }
